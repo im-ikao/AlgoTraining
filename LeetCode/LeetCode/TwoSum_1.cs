703699f6ef360676945a44ce7d02294ad0e63ccd
Aleksei Kostrykin <me@ikao.dev>
2024-06-02 15:30:27 +0300

    1. Two Sum
    Runtime: 118 ms (I' think it better then leetcode show)
    Memory: +-48.5 MB

---
+++ LeetCode/LeetCode/TwoSum_1.cs
@@ -0,0 +1,22 @@
+﻿namespace LeetCode;
+
+public class TwoSum_1 
+{
+    public static int[] TwoSum(int[] nums, int target)
+    {
+        var hashmap = new Dictionary<int, int>();
+
+        for (var i = 0; i < nums.Length; i++)
+        {
+            var complement = target - nums[i];
+            
+            if (hashmap.TryGetValue(complement, out var value))
+                return new int[] { value, i };
+
+            if (hashmap.ContainsKey(nums[i]) == false)
+                hashmap.Add(nums[i], i);
+        }
+
+        return null;
+    }
+}
