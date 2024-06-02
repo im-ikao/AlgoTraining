0c8db65d4b43b23a7a2954d7069059f84973aecb
Aleksei Kostrykin <me@ikao.dev>
2024-06-02 15:47:09 +0300

    2. Add Two Numbers
    Runtime: 64 ms  (I' think it better then leetcode show)
    Memory: 51.4 MB

---
+++ LeetCode/LeetCode/AddTwoNumbers_2.cs
@@ -0,0 +1,28 @@
+﻿namespace LeetCode;
+
+public class AddTwoNumbers_2
+{
+    public class ListNode
+    {
+        public int val;
+        public ListNode next;
+
+        public ListNode(int val = 0, ListNode next = null)
+        {
+            this.val = val;
+            this.next = next;
+        }
+    }
+
+    public ListNode AddTwoNumbers(ListNode l1, ListNode l2, int carry = 0)
+    {
+        if(l1 == null && l2 == null && carry == 0) 
+            return null;
+
+        var total = (l1?.val ?? 0) + (l2?.val ?? 0) + carry;
+        
+        carry = total / 10;
+        
+        return new ListNode(total % 10,  AddTwoNumbers(l1?.next, l2?.next, carry));
+    }
+}
